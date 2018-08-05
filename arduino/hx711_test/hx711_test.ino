502cbe3340fcae856cf45415a2cda9171ae9a53a
MaxAtoms <lisamith@outlook.de>
2018-08-05 19:26:01 +0200

    Improved speed of weight readings

--- arduino/hx711_test/hx711_test.ino
+++ arduino/hx711_test/hx711_test.ino
@@ -1,3 +1,10 @@
+/*
+ * This Arduino program is used to find the right value for the calibration_ratio
+ * 
+ * Use the HX711 library from:
+ * https://github.com/aguegu/ardulibs/tree/master/hx711
+ * The readme file of this repo also explains how to find the calibration_ratio 
+ */
 #include "hx711.h"
 
 Hx711 scale(A1, A0);
--- arduino/hx711_weighing/hx711_weighing.ino
+++ arduino/hx711_weighing/hx711_weighing.ino
@@ -1,24 +1,33 @@
 /*
  * Arduino project to read analog weight values from a connected HX711 chip.
  * 
- * Use the HX711 library from:
+ * To find the calibration_ratio use:
  * https://github.com/aguegu/ardulibs/tree/master/hx711
- * The readme file of this repo also explains how to find the calibration_ratio 
+ * 
+ * Datasheet of the HX711 chip:
+ * https://www.mouser.com/ds/2/813/hx711_english-1022875.pdf
  */
-#include "hx711.h" 
-Hx711 scale(A1, A0); // Data and Serial Clock pin
 
 double calibration_ratio = 13.21; // Adapt this value to fit your setup
 
+const uint8_t pin_dout = A1; // Data Out Pin
+const uint8_t pin_sck = A0; // Serial Clock Pin
+
+const byte REPETITIONS = 5; // Number of measurements taken for median weight value
+long offset;
+
 void setup() {
   Serial.begin(9600);
-  scale.setOffset(scale.averageValue());
-  scale.setScale(calibration_ratio);
+  
+  pinMode(pin_sck, OUTPUT);
+	pinMode(pin_dout, INPUT);
+
+  // Tare the scale
+  offset = averageValue(REPETITIONS);
 }
 
 void loop() {
-  Serial.println(scale.getGram(),0);
-
+  Serial.println(getGram(),0);
   checkForTare();
 }
 
@@ -30,7 +39,55 @@ void checkForTare() {
   }
 
   if(incomingByte == 'T') {
-    scale.setOffset(scale.averageValue());
+    offset = averageValue(REPETITIONS);
   }
 }
 
+float getGram()
+{
+	long val = (averageValue(REPETITIONS) - offset);
+	return (float) val / calibration_ratio;
+}
+
+long averageValue(byte times)
+{
+	long sum = 0;
+	for (byte i = 0; i < times; i++)
+	{
+		sum += getValue();
+	}
+
+	return sum / times;
+}
+
+long getValue()
+{
+	byte data[3];
+
+  // wait until there is data on DOUT
+	while (digitalRead(pin_dout))
+		;
+
+  // read the 24 bits from DOUT
+  // starting with the most significant bit
+	for (int j = 2; j>=0; j--)
+	{
+		for (char i = 7; i>=0; i--)
+		{
+			digitalWrite(pin_sck, HIGH);
+			bitWrite(data[j], i, digitalRead(pin_dout));
+			digitalWrite(pin_sck, LOW);
+		}
+	}
+
+  // set DOUT of the chip back to high
+	digitalWrite(pin_sck, HIGH);
+	digitalWrite(pin_sck, LOW);
+
+  // the hx711 sends 0x80 for MIN value
+  // in this case we set data to 0
+	data[2] ^= 0x80;
+
+	return ((uint32_t) data[2] << 16) | ((uint32_t) data[1] << 8)
+			| (uint32_t) data[0];
+}
